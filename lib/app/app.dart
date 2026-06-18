61f611a593c3d10d3b37687cd05413bf599530d6
obagwon <52205224+obagwon@users.noreply.github.com>
2026-06-18 19:37:35 +0900

    Add first screen startup splash

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -12,7 +12,60 @@ class SaihaeApp extends StatelessWidget {
       title: '사이해',
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
-      home: const MainTabScreen(),
+      home: const _StartupSplashGate(),
+    );
+  }
+}
+
+class _StartupSplashGate extends StatefulWidget {
+  const _StartupSplashGate();
+
+  @override
+  State<_StartupSplashGate> createState() => _StartupSplashGateState();
+}
+
+class _StartupSplashGateState extends State<_StartupSplashGate> {
+  static const _splashDuration = Duration(milliseconds: 1400);
+
+  var _showSplash = true;
+
+  @override
+  void initState() {
+    super.initState();
+    Future<void>.delayed(_splashDuration, () {
+      if (!mounted) {
+        return;
+      }
+
+      setState(() {
+        _showSplash = false;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_showSplash) {
+      return const _FirstScreenSplash();
+    }
+
+    return const MainTabScreen();
+  }
+}
+
+class _FirstScreenSplash extends StatelessWidget {
+  const _FirstScreenSplash();
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color(0xFFFFF8F2),
+      body: SizedBox.expand(
+        child: Image(
+          image: AssetImage('images/first_screen.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
     );
   }
 }
